b194801af3a4c89c9b0b7beca719240685e9980f
haidar-miqdad <haidarmiqdad2004@gmail.com>
2026-01-17 19:21:35 +0700

    Setup deployment workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/portfolio-2026/",
+    buildAssetsDir: "assets",
+  },
+
+  experimental: {
+    payloadExtraction: false,
+  },
   srcDir: "app/",
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
